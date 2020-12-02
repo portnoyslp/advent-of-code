c3c096698e1f7de12355f472fbc6364db713985b
portnoyslp <portnoy@portnoy.org>
2020-12-02 15:42:08 -0500

    Updated for more OO approach; reuse code where possible.

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,38 +1,44 @@
 import re
 from aocd import data
 
-def count_valid_passwords(input):
-    ok_words = 0
-    for line in input.splitlines():
-        if valid_password1(line):
-            ok_words += 1
-    return ok_words
+
+class PasswordChecker:
+    def __init__(self):
+        pass
+
+    def valid_password(self, line):
+        try:
+            low, high, needed_char, pw = self.parse_line(line)
+            count = pw.count(needed_char)
+            return low <= count <= high
+        except RuntimeWarning:
+            return False
+
+    @staticmethod
+    def parse_line(line):
+        match = re.match(r'(\d+)-(\d+) (.): (.+)', line)
+        if match is None:
+            raise RuntimeWarning('No matches found')
+        p1, p2, needed_char, pw = match.groups()
+        return int(p1), int(p2), needed_char, pw
+
+    @classmethod
+    def count_valid(cls, input_set):
+        ok_words = 0
+        for line in input_set.splitlines():
+            if cls().valid_password(line):
+                ok_words += 1
+        return ok_words
 
 
-def valid_password1(line):
-    match = re.match(r'(\d+)-(\d+) (.): (.+)', line)
-    if match is None:
-        return False
-    low, high, needed_char, pw = match.groups()
-    count = pw.count(needed_char)
-    return int(low) <= count <= int(high)
-
-
-def valid_password2(line):
-    match = re.match(r'(\d+)-(\d+) (.): (.+)', line)
-    if match is None:
-        return False
-    pos1, pos2, needed_char, pw = match.groups()
-    chars = [pw[i - 1] for i in [int(pos1), int(pos2)]]
-    return chars.count(needed_char) == 1
-
-
-def count_valid_passwords2(input):
-    ok_words = 0
-    for line in input.splitlines():
-        if valid_password2(line):
-            ok_words += 1
-    return ok_words
+class NewPasswordChecker(PasswordChecker):
+    def valid_password(self, line):
+        try:
+            pos1, pos2, needed_char, pw = self.parse_line(line)
+            chars = [pw[i - 1] for i in [pos1, pos2]]
+            return chars.count(needed_char) == 1
+        except RuntimeWarning:
+            return False
 
 
 test_input = '''
@@ -40,8 +46,8 @@ test_input = '''
 1-3 b: cdefg
 2-9 c: ccccccccc
 '''
-assert count_valid_passwords(test_input) == 2
-print(f"2a: {count_valid_passwords(data)}")
+assert PasswordChecker.count_valid(test_input) == 2
+print(f"2a: {PasswordChecker.count_valid(data)}")
 
-assert count_valid_passwords2(test_input) == 1
-print(f"2b: {count_valid_passwords2(data)}")
+assert NewPasswordChecker.count_valid(test_input) == 1
+print(f"2b: {NewPasswordChecker.count_valid(data)}")

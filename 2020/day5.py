bc848482c1b336fad4aa91abecc972bf82b01bb3
Stephen Peters <portnoy@portnoy.org>
2020-12-05 11:30:31 -0500

    Day 5 cleanup

--- 2020/day5.py
+++ 2020/day5.py
@@ -1,22 +1,24 @@
 from aocd import data
 import re
 
+
 def seat_id(spec):
     # regex replace to binary digits, and then parse as integer
-    spec = re.sub('F', '0', spec)
-    spec = re.sub('B', '1', spec)
-    spec = re.sub('R', '1', spec)
-    spec = re.sub('L', '0', spec)
+    spec = re.sub('[FL]', '0', spec)
+    spec = re.sub('[BR]', '1', spec)
     return int(spec, 2)
+
 
 def max_seat_id(input):
     return max([seat_id(spec) for spec in input.split()])
 
+
 def find_gap(input):
     seats = sorted([seat_id(spec) for spec in input.split()])
     for idx in seats[1:]:
-        if seats[idx] - seats[idx-1] > 1:
+        if seats[idx] - seats[idx - 1] > 1:
             return seats[idx] - 1
+
 
 assert seat_id('BFFFBBFRRR') == 567
 assert seat_id('FFFBBBFRRR') == 119

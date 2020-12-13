bdf5526d64c5c257785356bee28cd6cc5e890c0a
portnoyslp <portnoy@portnoy.org>
2020-12-13 09:39:22 -0500

    2020 Day 13

--- 2020/day13.py
+++ 2020/day13.py
@@ -1,4 +1,7 @@
+from functools import reduce
+
 from aocd import data
+
 
 def get_earliest_bus(input_str):
     buses, timestamp = parse_input(input_str)
@@ -24,15 +27,45 @@ def parse_input(input_str):
 
 def gold_coin_timestamp(input_str):
     buses, ignored = parse_input(input_str)
+    mods = []
+    vals = []
+    for i, bus in enumerate(buses):
+        if bus == 'x': continue
+        bus = int(bus)
+        mods.append(bus)
+        val = (bus - i) % bus
+        vals.append(val)
+    return chinese_remainder(mods, vals)
+
+
+def chinese_remainder(mods_list, vals_list):
+    sum = 0
+    prod = reduce(lambda a, b: a * b, mods_list)
+    for n_i, a_i in zip(mods_list, vals_list):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
 
 
 assert get_earliest_bus('''939
 7,13,x,x,59,x,31,19''') == 295
 print('13a: ', get_earliest_bus(data))
 
+assert gold_coin_timestamp('1\n17,x,13,19') == 3417
 assert gold_coin_timestamp('''939
 7,13,x,x,59,x,31,19''') == 1068781
-assert gold_coin_timestamp('1\n17,x,13,19') == 3417
 assert gold_coin_timestamp('1\n67,7,59,61') == 754018
 assert gold_coin_timestamp('1\n67,x,7,59,61') == 779210
 assert gold_coin_timestamp('1\n67,7,x,59,61') == 1261476

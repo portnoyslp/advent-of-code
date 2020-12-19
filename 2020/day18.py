1888e04c0cb9b9b1bcbb54c0cae5a3f9a851782a
portnoyslp <portnoy@portnoy.org>
2020-12-18 20:13:09 -0500

    2020 Day 18

--- 2020/day18.py
+++ 2020/day18.py
@@ -1,6 +1,7 @@
 from aocd import data
 import re
 
+
 class Homework:
     def __init__(self):
         self.tokens = []
@@ -14,26 +15,31 @@ class Homework:
 
     def process_tokens(self):
         while len(self.tokens) > 1:
-            self.process_top_operation()
+            self.tokens = self.simplify_token_list(self.tokens)
         return int(self.tokens.pop(0))
 
-    def process_top_operation(self):
-        val1 = self.fetch_operand()
-        op = self.fetch_op()
-        val2 = self.fetch_operand()
-        self.tokens.insert(0, val1 * val2 if op == '*' else val1 + val2)
+    def simplify_token_list(self, token_list):
+        for i in range(len(token_list) - 2):
+            # all ( x ) -> x
+            lp, val, rp = token_list[i:i + 3]
+            if lp == '(' and rp == ')':
+                token_list.pop(i)
+                token_list.pop(i + 1)
+                return token_list
+        for i in range(len(self.tokens) - 2):
+            # first num op num -> calculate
+            val1, op, val2 = token_list[i:i + 3]
+            if self.isnum(val1) and self.isnum(val2) and op in ['*', '+']:
+                val1 = int(val1)
+                val2 = int(val2)
+                del token_list[i:i + 3]
+                token_list.insert(i, val1 * val2 if op == '*' else val1 + val2)
+                return token_list
 
-    def fetch_op(self):
-        return self.tokens.pop(0)
+    @staticmethod
+    def isnum(token):
+        return type(token) is int or token[0].isdigit()
 
-    def fetch_operand(self):
-        if self.tokens[0] == '(':
-            # process parens
-            self.tokens.pop(0)
-            while self.tokens[1] != ')':
-                self.process_top_operation()
-            self.tokens.pop(1)
-        return int(self.tokens.pop(0))
 
 def process_all(input_str):
     return sum(map(lambda x: Homework().process(x), input_str.splitlines()))
@@ -47,14 +53,67 @@ assert Homework().process('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))') == 12240
 assert Homework().process('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2') == 13632
 print('18a: ', process_all(data))
 
+
 class Homework2(Homework):
     def __init__(self):
         Homework.__init__(self)
 
+    def simplify_token_list(self, token_list):
+        # If there's a ( num ) -> num
+        for i in range(len(token_list) - 2):
+            # all ( x ) -> x
+            lp, val, rp = token_list[i:i + 3]
+            if lp == '(' and rp == ')':
+                token_list.pop(i)
+                token_list.pop(i + 1)
+                return token_list
+        # Resolve paren'd equations down to singletons
+        token_list, altered = self.resolve_parens(token_list)
+        if altered:
+            return token_list
+        # Resolve + signs
+        for i in range(len(token_list) - 2):
+            # first num + num -> calculate
+            val1, op, val2 = token_list[i:i + 3]
+            if self.isnum(val1) and self.isnum(val2) and op == '+':
+                val1 = int(val1)
+                val2 = int(val2)
+                del token_list[i:i + 3]
+                token_list.insert(i, val1 + val2)
+                return token_list
+        # Finally, when we're down to just * signs, handle those
+        for i in range(len(token_list) - 2):
+            # first num * num -> calculate
+            val1, op, val2 = token_list[i:i + 3]
+            if self.isnum(val1) and self.isnum(val2) and op == '*':
+                val1 = int(val1)
+                val2 = int(val2)
+                del token_list[i:i + 3]
+                token_list.insert(i, val1 * val2)
+                return token_list
+
+    def resolve_parens(self, token_list):
+        # find innermost parens
+        paren_level = []
+        cur_level = 0
+        for token in token_list:
+            if token == '(':
+                cur_level += 1
+            if token == ')':
+                cur_level -= 1
+            paren_level.append(cur_level)
+        if sum(paren_level) == 0:
+            return token_list, False
+        max_level = max(paren_level)
+        sublist_idx = paren_level.index(max_level)
+        sublist_end = paren_level.index(max_level - 1, sublist_idx)
+        return token_list[:sublist_idx + 1] + self.simplify_token_list(
+            token_list[sublist_idx+1:sublist_end]) + token_list[sublist_end:], True
 
 
 def process_all2(input_str):
     return sum(map(lambda x: Homework2().process(x), input_str.splitlines()))
+
 
 assert Homework2().process('1 + 2 * 3 + 4 * 5 + 6') == 231
 assert Homework2().process('1 + (2 * 3) + (4 * (5 + 6))') == 51
